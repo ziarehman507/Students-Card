a182d28d48bd2cd9714ea238755156f2e16b9e98
Atif khan <ak8068269@gmail.com>
2023-07-14 18:52:47 -0700

    atif

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,16 @@ import React from 'react'
 import "./card.css"
 function Card1() {
   return (
-    <div className='div1'>Card1</div>
+    <div className='div1'>
+      <div className='h1'><h1>ATIF KHAN</h1></div>
+      <p1>FATHER NAME</p1>
+      <p2>AMIR KHAN</p2>
+      <div>
+        <p1>CNIC</p1>
+        <div className='cn'><p2>0201030405</p2></div>
+
+      </div>
+    </div>
   )
 }
 
